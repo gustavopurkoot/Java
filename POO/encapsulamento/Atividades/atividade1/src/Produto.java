1388c2d070abf680ce04b6727051262811b10028
gustavopurkoot <gustavopurkoot04053357@gmail.com>
2026-04-13 14:23:26 -0300

    Atualizar o Produto.java

--- POO/encapsulamento/Atividades/atividade1/src/Produto.java
+++ POO/encapsulamento/Atividades/atividade1/src/Produto.java
@@ -2,11 +2,11 @@ public class Produto {
     private String nome;
     private double preco;
     private int quantidade;
-    
+
     public Produto(String nome, double preco, int quantidade) {
         this.nome = nome;
-        this.preco = preco;
-        this.quantidade = quantidade;
+        setPreco(preco); // usando validação
+        setQuantidade(quantidade); // usando validação
     }
 
     public String getNome() {
@@ -22,11 +22,10 @@ public class Produto {
     }
 
     public void setPreco(double preco) {
-        if(preco > 0) {
+        if(preco >= 0) {
             this.preco = preco;
-        }
-        else {
-            System.out.println("Preço deve ser maior que zero.");
+        } else {
+            System.out.println("Preço não pode ser negativo.");
         }
     }
 
@@ -35,31 +34,30 @@ public class Produto {
     }
 
     public void setQuantidade(int quantidade) {
-        if(quantidade > 0) {
+        if(quantidade >= 0) {
             this.quantidade = quantidade;
-        }
-        else {
-            System.out.println("Quantidade deve ser maior que zero.");
+        } else {
+            System.out.println("Quantidade não pode ser negativa.");
         }
     }
 
     public double valorTotalEmEstoque(){
- 
-    return preco * quantidade;
+        return preco * quantidade;
     }
 
     public void adicionarEstoque(int quantidade){
-        this.quantidade += quantidade;
+        if(quantidade > 0) {
+            this.quantidade += quantidade;
+        } else {
+            System.out.println("Quantidade inválida para adicionar.");
+        }
     }
 
     public void removerEstoque(int quantidade){
-        if(quantidade <= this.quantidade) {
+        if(quantidade > 0 && quantidade <= this.quantidade) {
             this.quantidade -= quantidade;
-        }
-        else {
-            System.out.println("Quantidade a remover é maior do que a quantidade em estoque.");
+        } else {
+            System.out.println("Quantidade inválida para remover.");
         }
     }
 }
-
-
