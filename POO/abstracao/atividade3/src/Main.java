169a295b8c4033f75a2c3525b92ac84c5868990d
gustavopurkoot <gustavopurkoot04053357@gmail.com>
2026-04-13 12:36:02 -0300

    Atualizar o Main.java

--- POO/abstracao/atividade3/src/Main.java
+++ POO/abstracao/atividade3/src/Main.java
@@ -5,17 +5,20 @@ public class Main {
         Carro carro1 = new Carro();
         carro1.marca = "Ford";
         carro1.modelo = "Mustang";
-        carro1.velocidade = 0;
-        carro1.ligar();
-        carro1.acelerar();
-        carro1.acelerar();
-        carro1.status();
 
 
         Carro carro2 = new Carro();
         carro2.marca = "Chevrolet";
         carro2.modelo = "Camaro";
-        carro2.velocidade = 0;
+
+
+        carro1.status();
+        carro1.ligar();
+        carro1.acelerar();
+        carro1.acelerar();
+        carro1.status();
+
+        carro2.status();
         carro2.ligar();
         carro2.acelerar();
         carro2.acelerar();
